54a457a85013d85577b74b6961b7fe4b86a07d85
Tomoya Uchihara <e165728@ie.u-ryukyu.ac.jp>
2016-12-07 18:36:56 +0900

    version 1.2

--- src/main/jp/ac/uryukyu/ie/e165728/Main.java
+++ src/main/jp/ac/uryukyu/ie/e165728/Main.java
@@ -1,14 +1,17 @@
 package jp.ac.uryukyu.ie.e165728;
 
 public class Main {
-    public static void main(String[] args){
-        try{
+    public static void main(String[] args) {
+        String str = "3．14";
+        double value = Double.parseDouble(str);
+
+        /*try{
             String str = null;
            System.out.println(str.length());
         }
         catch(NullPointerException e){
             System.out.println("Warning: Exception detected");
             e.printStackTrace();
-        }
+        }*/
     }
 }
